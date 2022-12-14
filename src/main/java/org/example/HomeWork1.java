bb0c3ba6126f70bea61221f0857ab61da378dca1
Kazakov01 <kazakov.anton01@yandex.ru>
2022-12-14 21:29:16 +0300

    hw1 ver 1.1

--- src/main/java/org/example/HomeWork1.java
+++ src/main/java/org/example/HomeWork1.java
@@ -1,34 +1,35 @@
 package org.example;
 
 import java.util.Scanner;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class HomeWork1 {
 
-
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
         int rnd = random();
-        int num = sc.nextInt();
+        System.out.println(compare(rnd));
+    }
 
-        if (num < 101) {
-            System.out.println(compare(rnd, num));
-        } else {
-            throw new RuntimeException("What an idiot");
+    public static int random(){
+        return ThreadLocalRandom.current().nextInt(1, 100);
+    }
+
+    public static String compare(int rnd){
+        Scanner sc = new Scanner(System.in);
+        int num = 0;
+
+        while (num != rnd) {
+            num = sc.nextInt();
+
+            if (num > 101)
+                throw new RuntimeException("What an idiot");
+            if (num > rnd)
+                System.out.println("Your number is higher");
+            if (num < rnd)
+                System.out.println("Your number is less");
+            if (num == rnd)
+                return "THATS A MATCH!";
         }
-    }
-
-    public static int random() {
-        return (int) (Math.random() * 101);
-    }
-
-    public static String compare(int rnd, int num) {
-        if (num > rnd)
-            return "Your number is higher";
-        if (num < rnd)
-            return "Your number is less";
-        if (num == rnd)
-            return "THATS A MATCH!";
         return "";
     }
 }
-
