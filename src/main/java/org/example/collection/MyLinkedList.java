714336675a880086c87afde27b0680aad2b67621
Kazakov01 <kazakov.anton01@yandex.ru>
2023-02-11 15:44:06 +0300

    cw 11.02 mylinkedlist 2nd part

--- src/main/java/org/example/collection/MyLinkedList.java
+++ src/main/java/org/example/collection/MyLinkedList.java
@@ -1,5 +1,52 @@
 package org.example.collection;
 
-public class MyLinkedList {
 
+public class MyLinkedList {
+    private Node head;
+    private Node tail;
+    private int size;
+
+    public void addLast(String val) {
+        size++;
+        Node node = new Node(val);
+        if (head == null){
+            head = node;
+            tail = node;
+            return;
+        }
+        tail.next = node;
+        tail= tail.next;
+    }
+
+    // добавление в начале списка в качестве дз
+    public void addFirst(String val) {
+       size++;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public String toString() {
+        Node cur = head;
+        StringBuilder sb = new StringBuilder();
+
+        while (cur != null){
+            sb.append(cur.val).append(" -> ");
+            cur = cur.next;
+        }
+        sb.append("null");
+
+        return sb.toString();
+    }
+
+    private static class Node {
+        Node next;
+        String val;
+
+        public Node(String val) {
+            this.val = val;
+        }
+    }
 }
--- src/main/java/org/example/collection/MyLinkedListDemo.java
+++ src/main/java/org/example/collection/MyLinkedListDemo.java
@@ -4,21 +4,34 @@ public class MyLinkedListDemo {
     public static void main(String[] args) {
 //       abc qwe foo bar zxc
 
-        Node node5 = new Node("zxc", null);
-        Node node4 = new Node("bar", node5);
-        Node node3 = new Node("foo", node4);
-        Node node2 = new Node("qwe", node3);
-        Node head = new Node("abc", node2);
+//        Node node5 = new Node("zxc", null);
+//        Node node4 = new Node("bar", node5);
+//        Node node3 = new Node("foo", node4);
+//        Node node2 = new Node("qwe", node3);
+//        Node head = new Node("abc", node2);
 
-        node5.next = head;
+//        node5.next = head;
 
+        MyLinkedList mll = new MyLinkedList();
+        mll.addLast("abc");
+        mll.addLast("xyz");
+        mll.addLast("def");
+
+        System.out.println(mll);
 
     }
 
-    public static void print(Node node) {
-//вывод содержимого
-        //*вывести содержимое если цикл имеется
+    public static void print(Node head) {
+        Node cur = head;
+        while (cur != null) {
+            System.out.print(cur.value + " -> ");
+            cur = cur.next;
+        }
     }
+
+
+    public static void printWithCycle(Node head) {}
+    //функция успешно должна вывести список в консоль со списком
 
     public static class Node {
         String value;
