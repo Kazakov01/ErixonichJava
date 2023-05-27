8108c5b20dd26489c87aa3d96b6144d781c9386a
Kazakov01 <kazakov.anton01@yandex.ru>
2023-05-27 15:19:13 +0300

    cw 27.05 - threads mutex/locks explained

---
+++ src/main/java/org/example/multithreading/Demo4Locks.java
@@ -0,0 +1,37 @@
+package org.example.multithreading;
+
+public class Demo4Locks {
+
+    public static void main(String[] args) throws InterruptedException{
+        DataHolder dh = new DataHolder();
+        Object mutex = new Object();
+        Thread t1 = new Thread(()->{
+            for (int i = 0; i < 10000; i++) {
+                synchronized (mutex) {
+                    dh.num++;
+                }
+            }
+        });
+        Thread t2 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < 10000; i++) {
+                    synchronized (mutex) {
+                        dh.num++;
+                    }                }
+            }
+        });
+
+        t1.start();
+        t2.start();
+
+        t1.join();
+        t2.join();
+
+        System.out.println(dh.num);
+    }
+
+    public static class DataHolder {
+        int num;
+    }
+}
