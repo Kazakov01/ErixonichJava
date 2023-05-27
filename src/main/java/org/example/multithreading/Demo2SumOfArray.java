625e0d9742644c85e63f79c3d15aa7f70009be29
Kazakov01 <kazakov.anton01@yandex.ru>
2023-05-27 14:15:50 +0300

    cw 20.05 - threads exercise completed

--- src/main/java/org/example/multithreading/Demo2SumOfArray.java
+++ src/main/java/org/example/multithreading/Demo2SumOfArray.java
@@ -1,30 +1,66 @@
 package org.example.multithreading;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Random;
 
 public class Demo2SumOfArray {
 
     public static void main(String[] args) throws InterruptedException{
-        int[] arr = new int[1_000_000];
+        int[] arr = new int[100_000];
         Random rnd = new Random();
         for (int i = 0; i < arr.length; i++) {
-            arr[i] = rnd.nextInt(10_000);
+            arr[i] = rnd.nextInt(10);
         }
 
-        CountSumTask cst = new CountSumTask(arr, 0, arr.length/2);
-        CountSumTask cst2 = new CountSumTask(arr, arr.length/2, arr.length);
-        Thread t1 = new Thread(cst);
-        Thread t2 = new Thread(cst2);
-        t1.start();
-        t2.start();
-        t1.join();
-        t2.join();
-        System.out.println(cst.sum + cst2.sum);
+
+
+//        CountSumTask cst = new CountSumTask(arr, 0, arr.length/2);
+//        CountSumTask cst2 = new CountSumTask(arr, arr.length/2, arr.length);
+//        Thread t1 = new Thread(cst);
+//        Thread t2 = new Thread(cst2);
+//        t1.start();
+//        t2.start();
+//        t1.join();
+//        t2.join();
+        System.out.println(countSumMultithreaded(arr, Runtime.getRuntime().availableProcessors()));
+
+        System.out.println("Arrays.stream(arr).sum() = " + Arrays.stream(arr).sum());
     }
 
-    private static long countSumMultithreaded(int[] arr, int threadCount) {
-        // посчитать сумму массива в указанном количестве потоков учитывая равномерную их загруженность
-        return 0;
+    private static long countSumMultithreaded (int[] arr, int threadCount) throws InterruptedException{
+//        int begin = 0;
+//        int end = arr.length / threadCount-1;
+
+
+        long sum = 0;
+        int numbersPerTask = arr.length / threadCount;
+        List<CountSumTask> tasks = new ArrayList<>();
+        List<Thread> threads = new ArrayList<>();
+        for (int i = 0; i < threadCount; i++){
+            int end;
+            if (i == threadCount - 1) {
+                end = arr.length;
+            } else {
+                end = numbersPerTask * (i + 1);
+            }
+            CountSumTask cst = new CountSumTask(arr, numbersPerTask * i, end );
+            Thread t = new Thread(cst);
+            t.start();
+            threads.add(t);
+            tasks.add(cst);
+
+        }
+
+        for (Thread t : threads) {
+            t.join();
+        }
+
+        for (CountSumTask task : tasks) {
+            sum += task.sum;
+        }
+        return sum;
     }
 
     private static long countSum(int[] arr, int begin, int end) {
---
+++ src/main/java/org/example/multithreading/Demo3TextAnalysis.java
@@ -0,0 +1,8 @@
+package org.example.multithreading;
+
+public class Demo3TextAnalysis {
+    public static void main(String[] args) {
+        // скачать текстовый файл (книгу). Посчитать количество символов которые там имеются, посчитать среднюю
+        // длину слова, посчитать количество строк.
+    }
+}
