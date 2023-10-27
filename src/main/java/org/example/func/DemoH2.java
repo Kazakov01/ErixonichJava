6ee7f7c3e9765f68c313f97e7373746bb4c41917
Kazakov01 <kazakov.anton01@yandex.ru>
2023-10-27 13:29:40 +0300

    hw 11.10 localdate Streams Api

--- src/main/java/org/example/func/DemoH2.java
+++ src/main/java/org/example/func/DemoH2.java
@@ -1,25 +1,43 @@
 package org.example.func;
 
+import javax.swing.text.html.Option;
 import java.lang.constant.Constable;
+import java.time.DayOfWeek;
 import java.time.LocalDate;
 import java.util.*;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 public class DemoH2 {
 
     public static void main(String[] args) {
-        List<String> list = Arrays.asList("1999-09-23", "2007-01-23", "1985-02-01"
-                , "1977-12-21", "2013-06-11", "1999-08-12", "2023-09-23");
-//        LocalDate localDate = list.stream()
-//                .map(LocalDate::parse)
-//                .peek(ld -> System.out.println("1 " + ld.getMonth() + " " + ld.getYear()))
-//                .min(Comparator.naturalOrder())
-//                .orElseThrow();
+        List<String> list = Arrays.asList("1999-09-23", "1999-09-23", "1999-09-24", "1999-09-24", "2007-01-23", "1985-02-01"
+                , "1977-12-21", "2013-06-11", "1999-08-12", "2023-09-23", "2023-01-01", "2023-08-23");
 
-        List <LocalDate> localDate = list.stream()
+        List<LocalDate> localDate = list.stream()
                 .map(LocalDate::parse)
-                .peek(ld -> System.out.println("1 " + ld.getMonth() + " " + ld.getYear()))
+                .map((ld -> LocalDate.of(ld.getYear(), ld.getMonth(), 1)))
+                .distinct()
                 .toList();
+
+        List<LocalDate> localDateSorted = list.stream()
+                .map(LocalDate::parse)
+                .sorted(Comparator.naturalOrder())
+                .distinct()
+                .toList();
+
+        String mostPopularDay = list.stream()
+                .map(LocalDate::parse)
+                .map(ld -> ld.getDayOfWeek().name())
+                .collect(Collectors.groupingBy(dw -> dw, Collectors.counting()))
+                .entrySet().stream()
+                .max(Map.Entry.comparingByValue())
+                .map(en -> en.getKey())
+                .orElseThrow();
+
+        System.out.println(localDate);
+        System.out.println(localDateSorted);
+        System.out.println(mostPopularDay);
 
     }
 
